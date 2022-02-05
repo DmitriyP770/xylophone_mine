872a868960594b5d44eeb64a9f9ea8db536e189b
DmitriyP770 <pigasov.dm@gmail.com>
2022-02-05 19:08:48 +0300

    Summary

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  static AudioCache player = AudioCache();
+  void playSound(int i) {
+    player.play('note$i.wav');
+  }
+
+  String color;
+  Widget buildKey(Color colors, int num) {
+    return Expanded(
+      child: Container(
+        color: colors,
+        child: TextButton(
+          onPressed: () {
+            playSound(num);
+          },
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(Colors.red, 1),
+              buildKey(Colors.orange, 2),
+              buildKey(Colors.yellow, 3),
+              buildKey(Colors.green, 4),
+              buildKey(Colors.teal, 5),
+              buildKey(Colors.blue, 6),
+              buildKey(Colors.purple, 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
